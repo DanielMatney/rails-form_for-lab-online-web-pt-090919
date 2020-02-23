4dca62ec1afefe69aa5aca27fc977e2dd963e6a1
Daniel Matney <danieljmatney@gmail.com>
2020-02-23 03:48:38 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,23 +1,33 @@
 class SchoolClassesController < ApplicationController
   
-  def new
-    
-  end
-  
-  def create
-    
-  end
-  
   def show
-    
-  end
-  
-  def edit
-    
-  end
-  
-  def update
-    
+		@classes = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+	  @student = Student.new(student_params(:title, :description))
+	  @student.save
+	  redirect_to student_path(@student)
+	end
+
+	def update
+	  @student = Student.find(params[:id])
+	  @student.update(student_params(:name))
+	  redirect_to student_path(@student)
+	end
+
+	def edit
+	  @student = Student.find(params[:id])
+	end
+	
+	private
+	
+	def student_params(*args)
+  params.require(:student).permit(*args)
   end
   
 end
