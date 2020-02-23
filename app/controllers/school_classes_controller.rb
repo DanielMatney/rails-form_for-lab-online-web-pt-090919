bccf7642a0f0a9581c19a698d163e43965787e23
Daniel Matney <danieljmatney@gmail.com>
2020-02-23 03:44:47 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,29 +9,29 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def new
-		@post = Post.new
+		@post = Student.new
 	end
 
 	def create
-	  @post = Post.new(post_params(:title, :description))
+	  @post = Student.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(post_params(:title))
-	  redirect_to post_path(@post)
+	  @student = Student.find(params[:id])
+	  @student.update(post_params(:name))
+	  redirect_to post_path(@student)
 	end
 
 	def edit
-	  @post = Post.find(params[:id])
+	  @student = Student.find(params[:id])
 	end
 	
 	private
 	
 	def post_params(*args)
-  params.require(:post).permit(*args)
+  params.require(:student).permit(*args)
   end
   
 end
